56066e1136a3c18e786b93444157c3df50b5ce0f
Maciej <maciek.korsakowski@gmail.com>
2025-01-15 22:29:10 +0100

    add tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,32 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}", // Adjust paths based on your project structure
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      },
+      colors: {
+        grayCustom: '#ABABAB',
+      },
+      boxShadow: {
+        'custom': '2px 15px 50px rgba(0, 0, 0, 0.5)',
+      },
+      animation: {
+        'slide-in-right': 'slideInRight 0.8s ease-out forwards',
+      },
+      keyframes: {
+        slideInRight: {
+          '0%': {
+            transform: 'translateX(100%)',
+            opacity: 0,
+          },
+          '100%': {
+            transform: 'translateX(0)',
+            opacity: 1,
+          },
+        },
+      },
+    },
   },
   plugins: [],
 }
