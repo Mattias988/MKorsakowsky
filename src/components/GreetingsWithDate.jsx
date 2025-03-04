5b88e6537b589f66dcf85541c3f6b46886f50812
Maciej <maciek.korsakowski@gmail.com>
2025-03-04 17:37:12 +0100

    add date component

--- src/components/GreetingsWithDate.jsx
+++ src/components/GreetingsWithDate.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
+import ActualDate from "./ActualDate";
 
 const GreetingWithDate = () => {
-    const currentDate = new Date();
-    const day = currentDate.getDate();
-    const month = currentDate.toLocaleString('en-US', { month: 'short' });
 
     return (
         <div className="flex items-center space-x-4 text-black">
-            <p className="text-lg font-normal">Good morning</p>
-
-            <p className="text-lg font-light text-gray-600">{day} {month}</p>
+            <p className="text-lg font-medium">Good morning</p>
+            <ActualDate monthTypeDisplay={'short'}/>
         </div>
     );
 };
