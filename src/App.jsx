97016a94e9249cf1230c361440db5788a4ea52af
Maciej <maciek.korsakowski@gmail.com>
2025-03-04 17:36:03 +0100

    add styling to file

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
-import {Home} from './pages/Home';
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
 
 function App() {
-
     return (
-    <BrowserRouter>
-        <div className="w-full">
-            <div className="h-full">
-                <Routes>
-                    <Route path="/" element={<Home/>}/>
-                </Routes>
+        <div className="relative w-screen h-screen bg-whiteMain">
+            <div className="fixed inset-0 z-0 pointer-events-none">
+                <div className="absolute w-[400px] h-[400px] bg-[#ABABAB] rounded-full top-[-200px] right-[-200px] blur-[100px]" />
+                <div className="absolute w-[700px] h-[500px] bg-[#ABABAB] rounded-full blur-[100px] bottom-[-250px] left-[-250px]" />
+            </div>
+            <div className="relative z-10">
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                    </Routes>
+                </BrowserRouter>
             </div>
         </div>
-    </BrowserRouter>
-);
-};
+    );
+}
 
 export default App;
