d87fccd3ecca067b140413ac8f7d77ae6ea70e42
Maciej <maciek.korsakowski@gmail.com>
2025-03-07 21:32:27 +0100

    delete unnecessary components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,11 @@
 import React from "react";
-import { Header } from "../layouts/Header";
-import Footer from "../layouts/Footer";
 
 export const Home = () => {
     return (
         <div className="h-screen flex flex-col w-full bg-transparent ">
-            <Header className=" w-full bg-transparent" />
             <section className="w-screen h-full flex flex-1 justify-center items-center">
                 <h1 className="font-black pb-36 text-6xl capitalize">Hello, I'm Maciek. I'm fullstack developer.</h1>
             </section>
-            <Footer className="w-full  "/>
         </div>
     );
 };
