0f06af13f6782dbc122a164f1acdf17eb89ae44c
Maciej <maciek.korsakowski@gmail.com>
2025-03-07 21:31:18 +0100

    add route and add header and footer to every page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
+import {Skills} from "./pages/Skills";
+import {Header} from "./layouts/Header";
+import {Footer} from "./layouts/Footer";
 
 function App() {
     return (
@@ -9,11 +12,14 @@ function App() {
                 <div className="absolute w-[400px] h-[400px] bg-[#ABABAB] rounded-full top-[-200px] right-[-200px] blur-[100px]" />
                 <div className="absolute w-[700px] h-[500px] bg-[#ABABAB] rounded-full blur-[100px] bottom-[-250px] left-[-250px]" />
             </div>
-            <div className="relative z-10">
+            <div className="relative z-10 h-screen flex flex-col w-full bg-transparent">
                 <BrowserRouter>
+                    <Header />
                     <Routes>
                         <Route path="/" element={<Home />} />
+                        <Route path="/skills" element={<Skills />} />
                     </Routes>
+                    <Footer />
                 </BrowserRouter>
             </div>
         </div>
