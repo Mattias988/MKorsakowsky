7f45b429e17f67cddb1ec883db9110c3b8512e93
Maciej <maciek.korsakowski@gmail.com>
2025-03-09 09:12:29 +0100

    add content to skills page

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -3,8 +3,33 @@ import React from "react";
 export const Skills = () => {
 
     return (
-        <div className="h-screen flex flex-col w-full bg-transparent ">
+        <div className="h-screen flex flex-col w-full bg-transparent items-center pt-5">
+            <div className="flex flex-col gap-5">
+                <header className="w-full flex justify-center items-center font-semibold text-4xl">
+                    MY SKILLS
+                </header>
+                <span className="w-[10%] flex self-center border-b-2 border-black"></span>
+                <p className="text-gray-600 font-bold text-4lg">
+                    I really like experiment and invest my free time to extend my experience and knowledge about computer science, this is some of my area of interest ans skills
+                </p>
+                <section className="flex justify-center items-center">
+                    <div className="flex flex-col w-full">
+                        <div className="h-flex bg-grayCustom border-8 h-fit">
+                            <h2 className="font-bold text-xl">
+                                Frontend
+                            </h2>
+                            <p>Bardzo lubie fronta bo jest mega spoko, a jak go pisze to czuje sie jak sztywny gitcior takze nie ma co gadac fajna sprawa taki froncik porobic bla bla  bla bla  bla bla  bla bla  bla bla  bla bla  bla bla  bla bla </p>
+                        </div>
+                        <div>
 
+                        </div>
+                    </div>
+                    <div className="flex flex-col ">
+                    </div>
+                    <div className="flex flex-col">
+                    </div>
+                </section>
+            </div>
         </div>
     )
 }
