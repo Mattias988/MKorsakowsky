b8cb712e440656a721d2f19c511abc23a68ac783
Maciej <maciek.korsakowski@gmail.com>
2025-03-10 13:04:49 +0100

    add info item component

---
+++ src/components/InfoItem.jsx
@@ -0,0 +1,17 @@
+export const InfoItem = ({ Icon, text, href }) => {
+    return (
+        <li className="flex gap-4 items-center hover:bg-gray-100 p-2 rounded transition-all duration-300">
+            <a
+                className="flex gap-4 items-center"
+                href={href}
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+        <span className="font-bold transition-transform duration-300 hover:scale-105">
+          <Icon />
+        </span>
+                <span className="text-gray-700">{text}</span>
+            </a>
+        </li>
+    );
+};
