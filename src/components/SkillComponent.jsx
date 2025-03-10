2184fc98d68642222c0823201d31b0e523b8c4f3
Maciej <maciek.korsakowski@gmail.com>
2025-03-10 12:19:47 +0100

    change color text

--- src/components/SkillComponent.jsx
+++ src/components/SkillComponent.jsx
@@ -2,9 +2,9 @@ export const SkillComponent = ({title, desc, img, reverse}) => {
     return (
         <section className="flex justify-center items-center">
             <div className={`flex items-center h-full gap-4 w-full ${reverse ? "flex-row-reverse" : "flex-row"}`}>
-                <div className="w-[75%] bg-grayCustom border-8 h-full rounded-3xl p-4">
-                    <h2 className="font-bold text-xl text-whiteMain">{title}</h2>
-                    <p className="text-whiteMain">{desc}</p>
+                <div className="w-[75%] h-full bg-grayCustom rounded-3xl p-6">
+                    <h2 className="font-bold text-2xl text-gray-900">{title}</h2>
+                    <p className="text-md text-gray-900">{desc}</p>
                 </div>
 
                 <div className="w-[25%] h-full">
