b2ebe58e7377dcf7170647784fc6ff80492b1383
Maciej <maciek.korsakowski@gmail.com>
2025-03-10 11:46:27 +0100

    add display content by component

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import frontendSkillImg from '../assets/images/frontendCodeSkill.png'
+import {SkillComponent} from "../components/SkillComponent";
 
 export const Skills = () => {
+    const frontendDesc = "Bardzo lubie fronta bo jest mega spoko, a jak go pisze to czuje sie jak sztywny gitcior takze nie ma co gadac fajna sprawa taki froncik porobic bla bla  bla bla  bla bla  bla bla  bla bla  bla bla  bla bla  bla bla "
 
     return (
         <div className="h-screen flex flex-col w-full bg-transparent items-center pt-5">
@@ -9,26 +12,13 @@ export const Skills = () => {
                     MY SKILLS
                 </header>
                 <span className="w-[10%] flex self-center border-b-2 border-black"></span>
-                <p className="text-gray-600 font-bold text-4lg">
+                <p className="text-gray-600 font-bold text-4lg flex justify-center">
                     I really like experiment and invest my free time to extend my experience and knowledge about computer science, this is some of my area of interest ans skills
                 </p>
-                <section className="flex justify-center items-center">
-                    <div className="flex flex-col w-full">
-                        <div className="h-flex bg-grayCustom border-8 h-fit">
-                            <h2 className="font-bold text-xl">
-                                Frontend
-                            </h2>
-                            <p>Bardzo lubie fronta bo jest mega spoko, a jak go pisze to czuje sie jak sztywny gitcior takze nie ma co gadac fajna sprawa taki froncik porobic bla bla  bla bla  bla bla  bla bla  bla bla  bla bla  bla bla  bla bla </p>
-                        </div>
-                        <div>
-
-                        </div>
-                    </div>
-                    <div className="flex flex-col ">
-                    </div>
-                    <div className="flex flex-col">
-                    </div>
-                </section>
+                <SkillComponent title={"frontend"} desc={frontendDesc} img={frontendSkillImg} reverse={false}/>
+                <SkillComponent title={"frontend"} desc={"Test description"} img={frontendSkillImg} reverse={true}/>
+                <SkillComponent title={"frontend"} desc={"Test description"} img={frontendSkillImg} reverse={false}/>
+                <SkillComponent title={"frontend"} desc={"Test description"} img={frontendSkillImg} reverse={true}/>
             </div>
         </div>
     )
