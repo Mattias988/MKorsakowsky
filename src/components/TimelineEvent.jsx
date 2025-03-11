5cab812265964b64bf7045219dcd68f983f63026
Maciej <maciek.korsakowski@gmail.com>
2025-03-11 19:12:08 +0100

    add animation and change format of code

--- src/components/TimelineEvent.jsx
+++ src/components/TimelineEvent.jsx
@@ -1,24 +1,14 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 export const TimelineEvent = ({ event }) => {
     const [expanded, setExpanded] = useState(false);
 
     return (
         <div className="relative flex-1 text-center">
-            <div
-                className="
-          absolute
-          top-0
-          left-1/2
-          transform
-          -translate-x-1/2
-          -translate-y-1/2
-          w-4
-          h-4
-          bg-gray-700
-          rounded-full
-        "
-            />
+            <div className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                <div className="w-4 h-4 bg-gray-700 rounded-full animate-ping"></div>
+                <div className="w-4 h-4 bg-gray-700 rounded-full absolute top-0 left-0"></div>
+            </div>
 
             <p className="mt-5 font-semibold">{event.date}</p>
             <p className="text-sm">{event.title}</p>
@@ -31,23 +21,7 @@ export const TimelineEvent = ({ event }) => {
             </button>
 
             {expanded && (
-                <div
-                    className="
-            absolute
-            top-full
-            left-1/2
-            transform
-            -translate-x-1/2
-            mt-2
-            bg-gray-100
-            p-3
-            rounded
-            shadow
-            z-10
-            text-left
-            w-64
-          "
-                >
+                <div className="absolute top-full left-1/2 transform -translate-x-1/2 mt-2 bg-gray-100 p-3 rounded shadow z-10 text-left w-64">
                     <ul className="list-disc pl-5">
                         {event.technologies.map((tech, idx) => (
                             <li key={idx}>{tech}</li>
