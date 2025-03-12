1f7196b28ac659513f19a7eef34dd51b737a7fb5
Mattias988 <maciek.korsakowski@gmail.com>
2025-03-12 15:53:09 +0100

    add hover to headermenu

--- src/components/HeaderMenu.jsx
+++ src/components/HeaderMenu.jsx
@@ -1,24 +1,19 @@
 import {ReactComponent as HomeIcon} from "../assets/icons/homeIcon.svg"
-import {ReactComponent as SearchIcon} from "../assets/icons/searchIcon.svg"
-import {DropdownMenu} from "./DropdownMenu";
+ import {DropdownMenu} from "./DropdownMenu";
 import {Link} from "react-router-dom";
 
 const HeaderMenu = () => {
     return (
         <section className="flex items-center justify-end gap-2">
             <Link to={"/"}>
-                <div className="flex items-center justify-center bg-grayCustom rounded-3xl w-[150px] h-[50px] gap-2">
+                <div className="flex items-center justify-center bg-grayCustom rounded-3xl w-[150px] h-[50px] gap-2 hover:bg-black transition ">
                     <HomeIcon />
                     <p className="text-white">Home</p>
                 </div>
             </Link>
-            <div className="flex items-center justify-center bg-grayCustom rounded-3xl h-[50px] w-[150px] gap-3">
-                <DropdownMenu />
+            <div className="flex items-center justify-center bg-grayCustom rounded-3xl h-[50px] w-[150px] gap-3 hover:bg-black transition">
+                <DropdownMenu/>
             </div>
-            {/*<div className="flex items-center justify-center bg-grayCustom rounded-full h-[50px] w-[50px]">*/}
-            {/*    /!* here will be searching by words and letters *!/*/}
-            {/*    <SearchIcon onClick={() => window.alert("Work in progress")}/>*/}
-            {/*</div>*/}
         </section>
     )
 }
