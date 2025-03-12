cda4c06ba068e2dfe3e9896c1be9ac88c619aaf9
Mattias988 <maciek.korsakowski@gmail.com>
2025-03-12 10:52:52 +0100

    add navigation handle

--- src/components/DropdownMenu.jsx
+++ src/components/DropdownMenu.jsx
@@ -20,6 +20,11 @@ export const DropdownMenu = () => {
         setIsOpen(false);
     }, [location.pathname]);
 
+    const handleNavigation = (event, path) => {
+        event.preventDefault();
+        setIsOpen(false);
+        window.location.pathname = path;
+    };
     return (
         <div className="relative inline-block">
             <button
@@ -35,7 +40,7 @@ export const DropdownMenu = () => {
             {isOpen && (
                 <ul className="absolute left-0 mt-2 w-40 bg-white shadow-md rounded-lg overflow-hidden transition-all duration-300 opacity-100">
                     {filteredOptions.map(([path, name]) => (
-                        <li key={path} onClick={() => setIsOpen(false)} className="px-4 py-2 hover:bg-gray-200 cursor-pointer">
+                        <li key={path}  onClick={(event) => handleNavigation(event, path)} className="px-4 py-2 hover:bg-gray-200 cursor-pointer">
                             <Link to={path} replace>{name}</Link>
                         </li>
                     ))}
