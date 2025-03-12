d7d2af728bae66e303bbb7e5d07fe777fbdf33e1
Mattias988 <maciek.korsakowski@gmail.com>
2025-03-12 10:48:55 +0100

    change return by component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import {BrowserRouter, Routes, Route, useLocation} from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import { Home } from './pages/Home';
-import {Skills} from "./pages/Skills";
-import {Header} from "./layouts/Header";
-import {Footer} from "./layouts/Footer";
-import {Contact} from "./pages/Contact";
+import { Skills } from "./pages/Skills";
+import { Header } from "./layouts/Header";
+import { Footer } from "./layouts/Footer";
+import { Contact } from "./pages/Contact";
 
 function App() {
+    return (
+        <BrowserRouter>
+            <MainLayout />
+        </BrowserRouter>
+    );
+}
+
+const MainLayout = () => {
     const location = useLocation();
 
     return (
@@ -16,20 +24,18 @@ function App() {
                 <div className="absolute w-[700px] h-[500px] bg-[#ABABAB] rounded-full blur-[100px] bottom-[-250px] left-[-250px]" />
             </div>
             <div className="relative z-10 h-full flex flex-col w-full bg-transparent">
-                <BrowserRouter>
-                    <Header />
-                    <main className="max-w-[1480px] h-fit w-full mx-auto flex-grow">
-                        <Routes key={location.pathname}>
-                            <Route path="/" element={<Home />} />
-                            <Route path="/skills" element={<Skills />} />
-                            <Route path="/contact" element={<Contact />} />
-                        </Routes>
-                    </main>
-                    <Footer />
-                </BrowserRouter>
+                <Header />
+                <main className="max-w-[1480px] h-fit w-full mx-auto flex-grow">
+                    <Routes key={location.pathname}>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/skills" element={<Skills />} />
+                        <Route path="/contact" element={<Contact />} />
+                    </Routes>
+                </main>
+                <Footer />
             </div>
         </div>
     );
-}
+};
 
 export default App;
