38da2ac2c6aebbe4cc76d4e6201734802cbfb2b4
Mattias988 <maciek.korsakowski@gmail.com>
2025-03-12 15:52:54 +0100

    add animations to header

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,12 +1,17 @@
 import GreetingsWithDate from "../components/GreetingsWithDate";
 import HeaderMenu from "../components/HeaderMenu";
-
+import { motion } from "framer-motion";
 
 export const Header = () => {
     return (
-        <header className="w-full px-8 pt-6 flex bg-transparent justify-between">
+        <motion.header
+            initial={{ opacity: 0, y: -30 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, ease: "easeOut" }}
+            className="w-full px-8 pt-6 flex bg-transparent justify-between"
+        >
             <GreetingsWithDate />
             <HeaderMenu />
-        </header>
-    )
-}
+        </motion.header>
+    );
+};
