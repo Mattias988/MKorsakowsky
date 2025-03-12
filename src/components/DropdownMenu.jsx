36e7ab49c4abe4d693b7407a51c7ca5ab4031319
Mattias988 <maciek.korsakowski@gmail.com>
2025-03-12 10:49:21 +0100

    add close menu after pick option

--- src/components/DropdownMenu.jsx
+++ src/components/DropdownMenu.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { ReactComponent as ArrowDown } from "../assets/icons/arrowDown.svg";
 
@@ -14,29 +14,29 @@ export const DropdownMenu = () => {
     };
 
     const currentLocation = pathMap[location.pathname] || "Home";
-
     const filteredOptions = Object.entries(pathMap).filter(([path, name]) => name !== currentLocation);
 
-    const handleLinkClick = () => {
+    useEffect(() => {
         setIsOpen(false);
-    };
+    }, [location.pathname]);
 
     return (
-        <div
-            className="relative inline-block"
-        >
-            <button className="flex items-center text-white px-4 py-2 gap-3 rounded-full">
+        <div className="relative inline-block">
+            <button
+                onClick={() => setIsOpen((prev) => !prev)}
+                className="flex items-center text-white px-4 py-2 gap-3 rounded-full"
+            >
                 {currentLocation}
-                <div onClick={() => setIsOpen((prev) => !prev)} className="rounded-full bg-white w-[35px] h-[35px] flex justify-center items-center">
-                    <ArrowDown className={`${isOpen ? "rotate-180" : ""}`}/>
+                <div className="rounded-full bg-white w-[35px] h-[35px] flex justify-center items-center">
+                    <ArrowDown className={`${isOpen ? "rotate-180" : ""}`} />
                 </div>
             </button>
 
             {isOpen && (
                 <ul className="absolute left-0 mt-2 w-40 bg-white shadow-md rounded-lg overflow-hidden transition-all duration-300 opacity-100">
                     {filteredOptions.map(([path, name]) => (
-                        <li key={path} className="px-4 py-2 hover:bg-gray-200 cursor-pointer">
-                            <Link to={path} onClick={handleLinkClick}>{name}</Link>
+                        <li key={path} onClick={() => setIsOpen(false)} className="px-4 py-2 hover:bg-gray-200 cursor-pointer">
+                            <Link to={path} replace>{name}</Link>
                         </li>
                     ))}
                 </ul>
