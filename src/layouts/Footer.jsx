ffdb9d89b7f860137db1ae782154b8ea7b9c4bd7
Maciej <maciek.korsakowski@gmail.com>
2025-03-18 12:27:09 +0100

    add responsive display

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -13,12 +13,12 @@ export const Footer = () => {
             className="fixed bottom-0 left-0 w-full px-8 pb-6 flex justify-end items-center bg-transparent"
         >
             {temperature !== null ? (
-                <div className="text-lg flex gap-2 font-normal text-gray-700">
+                <div className="text-sm sm:text-lg flex gap-2 font-normal text-gray-700">
                     <ActualDate monthTypeDisplay={"numeric"} />
                     {temperature}°C
                 </div>
             ) : (
-                <div className="text-lg flex font-normal px-8 gap-x-5 text-gray-500">
+                <div className="text-sm sm:text-lg flex font-normal px-8 gap-x-5 text-gray-500">
                     <ActualDate monthTypeDisplay={"numeric"} />
                     <p>Ładowanie temperatury...</p>
                 </div>
