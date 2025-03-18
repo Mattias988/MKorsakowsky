16945e19e6c26f101b943d371a8beadaaae0fc8c
Maciej <maciek.korsakowski@gmail.com>
2025-03-18 12:27:14 +0100

    add responsive display

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -8,7 +8,7 @@ export const Header = () => {
             initial={{ opacity: 0, y: -30 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 1, ease: "easeOut" }}
-            className="w-full px-8 pt-6 flex bg-transparent justify-between"
+            className="w-full px-8 pt-6 flex bg-transparent justify-between z-40"
         >
             <GreetingsWithDate />
             <HeaderMenu />
