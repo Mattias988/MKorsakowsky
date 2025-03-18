39b2cee7855ee67bd21d99684a4752b8de513e97
Maciej <maciek.korsakowski@gmail.com>
2025-03-18 12:27:12 +0100

    add responsive display

--- src/components/GreetingsWithDate.jsx
+++ src/components/GreetingsWithDate.jsx
@@ -5,7 +5,7 @@ const GreetingWithDate = () => {
 
     return (
         <div className="flex flex-col justify-center sm:flex-row sm:items-center sm:space-x-4 text-black">
-            <p className="text-lg font-medium">Good morning</p>
+            <p className="text-sm sm:text-lg font-medium">Good morning</p>
             <ActualDate monthTypeDisplay={'short'}/>
         </div>
     );
