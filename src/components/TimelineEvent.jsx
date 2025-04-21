eab5bf205214a74ff243c0285fd7d9418352be86
Maciej <maciek.korsakowski@gmail.com>
2025-04-21 19:21:17 +0200

    add list of tech on large screens

--- src/components/TimelineEvent.jsx
+++ src/components/TimelineEvent.jsx
@@ -1,10 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { motion, AnimatePresence } from "framer-motion";
+import { ReactComponent as ArrowDown } from "../assets/icons/arrowDown.svg";
 
 export const TimelineEvent = ({ event }) => {
     const [expanded, setExpanded] = useState(false);
-    const [isOpen, setIsOpen] = useState(false);
+    const [isExpanded, setIsExpanded] = useState(false);
     const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+    const [openTechIndex, setOpenTechIndex] = useState(false);
+    const dropdownRef = useRef(null);
 
     useEffect(() => {
         const handleResize = () => {
@@ -15,6 +18,22 @@ export const TimelineEvent = ({ event }) => {
         return () => window.removeEventListener("resize", handleResize);
     }, []);
 
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+                setExpanded(false);
+                setOpenTechIndex(null);
+            }
+        };
+
+        if (expanded) {
+            document.addEventListener("mousedown", handleClickOutside);
+        }
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [expanded]);
     return (
         <div className="relative flex-1 w-full text-center">
             {isMobile && (
@@ -24,13 +43,13 @@ export const TimelineEvent = ({ event }) => {
             )}
             <div
                 className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer"
-                onClick={() => isMobile && setIsOpen(prev => !prev)}
+                onClick={() => isMobile && setIsExpanded(prev => !prev)}
             >
                 <div className="w-4 h-4 bg-gray-700 rounded-full animate-ping"></div>
                 <div className="w-4 h-4 bg-gray-700 rounded-full absolute top-0 left-0"></div>
             </div>
 
-            {(isOpen || !isMobile) && (
+            {(isExpanded || !isMobile) && (
                 <>
                     <p className="mt-5 w-20 sm:w-full font-medium text-xs sm:text-base sm:font-semibold text-gray-900">{event.date}</p>
                     <p className="text-[10px] sm:text-base text-gray-800">{event.title}</p>
@@ -45,16 +64,35 @@ export const TimelineEvent = ({ event }) => {
                     <AnimatePresence>
                         {expanded && (
                             <motion.div
+                                ref={dropdownRef}
                                 initial={{ opacity: 0, y: -10, scale: 0.95 }}
                                 animate={{ opacity: 1, y: 0, scale: 1 }}
                                 exit={{ opacity: 0, y: -10, scale: 0.95 }}
                                 transition={{ duration: 0.3 }}
-                                className="absolute top-full left-[-30px] transform -translate-x-1/2 mt-2 bg-grayCustom shadow-lg rounded-xl backdrop-blur-md border border-gray-400 p-4 text-left w-30 text-xs sm:text-lg sm:w-64 z-10"
+                                className="absolute top-20 left-0 transform -translate-x-1/2 -translate-y-1/2 mt-2 bg-transparent shadow-lg rounded-xl backdrop-blur-lg border border-gray-400 p-4 text-left w-30 text-xs sm:text-lg sm:w-64 lg:w-96 z-10"
                             >
-                                <ul className="list-disc pl-5 text-white font-medium">
+                                <ul className="pl-5 text-gray-800 font-medium list-none">
                                     {event.technologies.map((tech, idx) => (
-                                        <li key={idx} className="hover:text-black transition-all duration-200">
-                                            {tech}
+                                        <li key={idx} className="hover:text-gray-400 transition-all duration-200">
+                                            <div
+                                                className="flex justify-between items-center cursor-pointer"
+                                                onClick={() => {if(!isMobile) setOpenTechIndex(prev => prev === idx ? null : idx)}}
+                                            >
+                                                {tech}
+                                                {!isMobile ? <ArrowDown className={`${openTechIndex === idx ? "rotate-180" : "rotate-0"} transition-transform duration-200`} /> : null}
+                                            </div>
+                                            {openTechIndex === idx && !isMobile && (
+                                                <motion.div
+                                                    initial={{ opacity: 0, y: -10, scale: 0.95 }}
+                                                    animate={{ opacity: 1, y: 0, scale: 1 }}
+                                                    exit={{ opacity: 0, y: -10, scale: 0.95 }}
+                                                    transition={{ duration: 0.3 }}
+                                                    >
+                                                    <p className="mt-2 text-gray-600 bg-gradient-to-r from-grayCustom to-whiteMain rounded-lg p-2 backdrop-blur-lg text-sm">
+                                                        Tutaj możesz dodać info o technologii: <strong>{tech}</strong>
+                                                    </p>
+                                                </motion.div>
+                                            )}
                                         </li>
                                     ))}
                                 </ul>
